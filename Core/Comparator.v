95e84ba0722d60e35c720e0a4a2b255c78e8d6ee
DEVAN9105 <devansun299@gmail.com>
2026-01-15 13:07:36 +0800

    Update Comparator.v

--- Core/Comparator.v
+++ Core/Comparator.v
@@ -1,38 +1,32 @@
 `timescale 1ns / 1ps
 
-//delay = 1
+//combinational
 
 module Comparator(
-    input CLK,
-    input en,
     input signed [15:0] comp_a,
     input signed [15:0] comp_b,
     input signed [15:0] comp_c,
     output reg signed [15:0] comp_out
     );
     
-    always@(posedge CLK) begin
-        if(en == 1) begin
-            if(comp_a >= comp_b) begin
-                if(comp_a >= comp_c) begin
-                    comp_out <= comp_a;
-                end
-                else begin
-                    comp_out <= comp_c;
-                end
+    always@(*) begin
+        if(comp_a >= comp_b) begin
+            if(comp_a >= comp_c) begin
+                comp_out = comp_a;
             end
             else begin
-                if(comp_b >= comp_c) begin
-                    comp_out <= comp_b;
-                end
-                else begin
-                    comp_out <= comp_c;
-                end
+                comp_out = comp_c;
             end
         end
         else begin
-            comp_out <= 0;
+            if(comp_b >= comp_c) begin
+                comp_out = comp_b;
+            end
+            else begin
+                comp_out = comp_c;
+            end
         end
     end
     
 endmodule
+
