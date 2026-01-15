c1e371fe9bc771598d1780f8a840e9a35caf6d82
DEVAN9105 <devansun299@gmail.com>
2026-01-15 13:16:47 +0800

    Update PE.v

--- Core/PE.v
+++ Core/PE.v
@@ -11,32 +11,13 @@ module PE(
     input signed [15:0] PE_B,
     output reg signed [31:0] PE_mac_out
     );
-    wire valid; //Anding buffer
-    reg signed [15:0] A,B; //mult_16 input
     wire signed [31:0] P; //mult_16 output
+    
     //Mode define
     parameter MAC = 1'b0, GAP = 1'b1;
     
-    //Anding A and B
-    assign valid = (|PE_A) && (|PE_B); //reduction OR
-    
-    //Select A and B's value
-    always@(*) begin
-        case(valid)
-            1'b1: begin
-                A = PE_A; B = PE_B;
-            end
-            1'b0: begin
-                A = 16'b0; B = 16'b0;
-            end
-            default: begin
-                A = 16'b0; B = 16'b0;
-            end
-        endcase
-    end
-    
     //mult_16
-    mult_16 mult(CLK,A,B,PE_en,rst,P);
+    mult_16 mult(CLK,PE_A,PE_B,PE_en,rst,P);
     
     //shift_right_4
     wire signed [31:0] A_shift;
