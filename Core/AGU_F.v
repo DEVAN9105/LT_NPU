feed61d4410dfab5133cc43e0f66ae98c6f1c22b
DEVAN9105 <devansun299@gmail.com>
2026-01-17 02:32:02 +0800

    Update AGU_F.v

--- Core/AGU_F.v
+++ Core/AGU_F.v
@@ -7,8 +7,9 @@ module AGU_F(
     input padding,           // 0: PW Mode, 1: DW Mode
     input [7:0] AGU_initial, // Y initial
     input [6:0] width_in,    // Map Width (0~127)
+    input [6:0] width_out,
     input [7:0] ch_in,       // DW ch addr end
-    input [1:0] AGU_stride,  // Stride
+    input [1:0] stride,  // Stride
     input [1:0] AGU_offset_X,// DW Max Offset
     input [8:0] AGU_offset_Y,// PW Max Offset (last channel's first addr)
     output reg [7:0] faddr,
@@ -19,18 +20,16 @@ module AGU_F(
     wire [7:0] ch_stride = width_in + 1; //width + 1 (Max 128)
     
     // Counters
+    reg [7:0] count,next_count;
     reg [7:0] X, next_X;
-    reg [8:0] Y, next_Y;
     reg [1:0] offset_X, next_offset_X;
     reg [8:0] offset_Y, next_offset_Y;
 
     //Boundary Logic (DW & MaxPooling & conv_1)
     reg [8:0] addr_X; //real addr
-    reg [8:0] addr_Y; //real addr
     always@(*) begin
-        addr_Y = Y + offset_Y;
         if(padding == 1) begin
-            if((X + offset_X) == 0 || (X + offset_X) > ch_stride) begin
+            if(((X + offset_X) == 0) || ((X + offset_X) > ch_stride)) begin
                 addr_X = X + offset_X;
             end
             else begin
@@ -43,12 +42,12 @@ module AGU_F(
     end
     
     //boundary signal
-    always@(*) begin
+    always@(posedge CLK) begin
         if (padding == 1) begin
-            if ( ((X + offset_X) == 0) || ((X + offset_X) > ch_stride) ) boundary = 1;
-            else boundary = 0;
+            if ( ((X + offset_X) == 0) || (addr_X > width_in) ) boundary <= 1;
+            else boundary <= 0;
         end
-        else boundary = 0;
+        else boundary <= 0;
     end
     
     // AGU Logic
@@ -56,36 +55,28 @@ module AGU_F(
     always@(*) begin
         // Default Assignments (防止 Latch)
         next_X = X;
-        next_Y = Y;
+        next_count = count;
         next_offset_X = offset_X;
         next_offset_Y = offset_Y;
         next_done = done;
         if ((offset_Y < AGU_offset_Y) && (~padding)) begin
                 next_offset_Y = offset_Y + ch_stride;
-                //next_Y = Y + 1;
         end
         else begin
-            //next_Y = Y;
             next_offset_Y = 0;
             if(offset_X < AGU_offset_X) begin
                 next_offset_X = offset_X + 1;
-                next_Y = Y;
             end
             else begin
                 next_offset_X = 0;
-                if( (X + offset_X) < (ch_stride + (padding << 1) - 1) ) begin //DW stride
-                    next_X = X + AGU_stride;
-                    next_Y = Y;
+                if( count < width_out ) begin //DW stride
+                    next_X = X + stride;
+                    next_count = count + 1;
                 end
                 else begin
                     next_X = 0;
-                    if(Y < ch_in) begin
-                        next_Y = Y + ch_stride;
-                    end
-                    else begin
-                        next_Y = Y;
-                        next_done = 1;
-                    end
+                    next_count = count;
+                    next_done = 1;
                 end
             end
         end
@@ -96,7 +87,7 @@ module AGU_F(
     always@(posedge CLK) begin
         if(rst) begin
             X <= 0;
-            Y <= 0;
+            count <= 0;
             offset_X <= 0;
             offset_Y <= 0;
             faddr <= AGU_initial;
@@ -104,7 +95,7 @@ module AGU_F(
         end
         else if(en) begin
             X <= next_X;
-            Y <= next_Y;
+            count <= next_count;
             offset_X <= next_offset_X;
             offset_Y <= next_offset_Y;
             done <= next_done;
@@ -114,9 +105,10 @@ module AGU_F(
                 faddr <= 0;
             end
             else begin // PW
-                faddr <= AGU_initial + addr_X + addr_Y;
+                faddr <= AGU_initial + addr_X + offset_Y;
             end
         end
     end
+    
     
 endmodule 
