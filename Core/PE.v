7ccd570585f6ef1069cd198998c10e2cb03e29e6
DEVAN9105 <devansun299@gmail.com>
2026-01-19 19:34:09 +0800

    Update PE.v

--- Core/PE.v
+++ Core/PE.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
-//delay 3
+//delay = 4 cycle
 
 module PE(
     input CLK,
-    input PE_en,
+    input en,
     input rst,
     input PE_mode,
     input signed [15:0] PE_A,
@@ -15,14 +15,33 @@ module PE(
     
     //Mode define
     parameter MAC = 1'b0, GAP = 1'b1;
-    
+
     //mult_16
-    mult_16 mult(CLK,PE_A,PE_B,PE_en,rst,P);
+    mult_16 mult(.CLK(CLK),
+                 .A(PE_A),
+                 .B(PE_B),
+                 .CE(en),
+                 .SCLR(rst),
+                 .P(P)
+    );
     
     //shift_right_4
-    wire signed [31:0] A_shift;
-    assign A_shift = $signed({{16{PE_A[15]}}, PE_A}) >>> 4; //>>> is for signed bit
-    
+    reg signed [31:0] A_shift_0, A_shift_1, A_shift;
+    always@(posedge CLK) begin
+        if(rst==1) A_shift_0 <= 0;
+        else A_shift_0 <= $signed({{16{PE_A[15]}}, PE_A}) >>> 4; //>>> is for signed bit
+    end
+
+    always@(posedge CLK) begin
+        if(rst==1) A_shift_1 <= 0;
+        else A_shift_1 <= A_shift_0;
+    end
+
+    always@(posedge CLK) begin
+        if(rst==1) A_shift <= 0;
+        else A_shift <= A_shift_1;
+    end
+
     //output mux
     reg signed [31:0]out_buffer;
     always@(*) begin
@@ -37,7 +56,7 @@ module PE(
     always@(posedge CLK) begin
         if(rst==1) PE_mac_out <= 0;
         else begin
-            if(PE_en==1) PE_mac_out <= out_buffer;
+            if(en==1) PE_mac_out <= out_buffer;
             else PE_mac_out <= PE_mac_out;
         end
     end
