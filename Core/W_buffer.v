99fb9b7d160bb8976fa6cf8144a38679923570ea
DEVAN9105 <devansun299@gmail.com>
2026-01-19 21:01:43 +0800

    debug

--- Core/W_buffer.v
+++ Core/W_buffer.v
@@ -5,7 +5,8 @@
 module W_buffer(
     input CLK,
     input rst,
-    input W_buffer_en,
+    input en,
+    input bias_en,
     input [63:0]wdata_0,
     input [63:0]wdata_1,
     input [63:0]wdata_2,
@@ -13,7 +14,11 @@ module W_buffer(
     output reg [63:0]PE_win_0,
     output reg [63:0]PE_win_1,
     output reg [63:0]PE_win_2,
-    output reg [63:0]PE_win_3
+    output reg [63:0]PE_win_3,
+    output reg [31:0]acc_0_bias,
+    output reg [31:0]acc_1_bias,
+    output reg [31:0]acc_2_bias,
+    output reg [31:0]acc_3_bias
     );
     
     always@(posedge CLK) begin
@@ -24,7 +29,7 @@ module W_buffer(
             PE_win_3 <= 0;
         end
         else begin
-            if(W_buffer_en == 0) begin
+            if(en == 0) begin
                 PE_win_0 <= PE_win_0;
                 PE_win_1 <= PE_win_1;
                 PE_win_2 <= PE_win_2;
@@ -35,6 +40,29 @@ module W_buffer(
                 PE_win_1 <= wdata_1;
                 PE_win_2 <= wdata_2;
                 PE_win_3 <= wdata_3;
+            end
+        end
+    end
+
+    always@(posedge CLK) begin
+        if(rst == 1) begin
+            acc_0_bias <= 0;
+            acc_1_bias <= 0;
+            acc_2_bias <= 0;
+            acc_3_bias <= 0;
+        end
+        else begin
+            if(bias_en == 0) begin
+                acc_0_bias <= acc_0_bias;
+                acc_1_bias <= acc_1_bias;
+                acc_2_bias <= acc_2_bias;
+                acc_3_bias <= acc_3_bias;
+            end
+            else begin
+                acc_0_bias <= wdata_0[63:32];
+                acc_1_bias <= wdata_1[63:32];
+                acc_2_bias <= wdata_2[63:32];
+                acc_3_bias <= wdata_3[63:32];
             end
         end
     end
