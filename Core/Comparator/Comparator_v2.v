10f6112304bc5acc1e7cb58ace52c63df093bba6
DEVAN9105 <devansun299@gmail.com>
2026-01-24 09:10:07 +0800

    Update Comparator_v2.v

--- Core/Comparator/Comparator_v2.v
+++ Core/Comparator/Comparator_v2.v
@@ -5,26 +5,15 @@
 module Comparator(
     input signed [15:0] comp_a,
     input signed [15:0] comp_b,
-    input signed [15:0] comp_c,
     output reg signed [15:0] comp_out
     );
     
     always@(*) begin
         if(comp_a >= comp_b) begin
-            if(comp_a >= comp_c) begin
-                comp_out = comp_a;
-            end
-            else begin
-                comp_out = comp_c;
-            end
+            comp_out = comp_a;
         end
         else begin
-            if(comp_b >= comp_c) begin
-                comp_out = comp_b;
-            end
-            else begin
-                comp_out = comp_c;
-            end
+            comp_out = comp_b;
         end
     end
     
