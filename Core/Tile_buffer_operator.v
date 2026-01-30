814ac325a975a2310e0dcd4856939d93cf4c652c
DEVAN9105 <devansun299@gmail.com>
2026-01-30 19:18:53 +0800

    Update Tile_buffer_operator.v

--- Core/Tile_buffer_operator.v
+++ Core/Tile_buffer_operator.v
@@ -7,280 +7,498 @@ module Tile_buffer_operator(
     input en,
     input rst,
     // tile assign
-    input [11:0] tile_Core,
-    input [8:0] tile_CIU,
+    input [2:0] tile_sel_cycle,
+    input [19:0] tile_assign,
     // tile in
     input valid_in,
     input [7:0] addr_in,
     input [63:0] din_in,
     // tile cycle
-    input valid_cycle_0,
-    input valid_cycle_1,
-    input [7:0] addr_cycle_0,
-    input [7:0] addr_cycle_1,
-    input [63:0] din_cycle_0,
-    input [63:0] din_cycle_1,
+    input valid_cycle_a,
+    input valid_cycle_b,
+    input [7:0] addr_cycle_a,
+    input [7:0] addr_cycle_b,
+    input [63:0] din_cycle_a,
+    input [63:0] din_cycle_b,
     output [63:0] dout_cycle,
     // tile out
     input [7:0] addr_out,
     output [63:0] dout_out,
     // tile cal
     input [7:0] addr_cal,
-    output reg [63:0] dout_cal_0,
-    output reg [63:0] dout_cal_1,
-    output reg [63:0] dout_cal_2,
-    output reg [63:0] dout_cal_3,
+    output reg [63:0] tile_1,
+    output reg [63:0] tile_2,
+    output reg [63:0] tile_3,
+    output reg [63:0] tile_4,
+    output reg [63:0] tile_5,
+    output reg [63:0] tile_6,
     // tile store
     input valid_store,
     input [7:0] addr_store,
     input [63:0] din_store
     );
 
-    ////////// decode //////////
-    // ena
-    reg [6:0] ena_in, en_cycle, ena_out, ena;
-    always@(*) begin
-        // tile in
-        case(tile_CIU[8:6])
-            1: ena_in = 7'b0000001;
-            2: ena_in = 7'b0000010;
-            3: ena_in = 7'b0000100;
-            4: ena_in = 7'b0001000;
-            5: ena_in = 7'b0010000;
-            6: ena_in = 7'b0100000;
-            7: ena_in = 7'b1000000;
-            default: ena_in = 7'b0000000;
-        endcase
-        // tile cycle
-        case(tile_CIU[5:3])
-            1: en_cycle = 7'b0000001;
-            2: en_cycle = 7'b0000010;
-            3: en_cycle = 7'b0000100;
-            4: en_cycle = 7'b0001000;
-            5: en_cycle = 7'b0010000;
-            6: en_cycle = 7'b0100000;
-            7: en_cycle = 7'b1000000;
-            default: en_cycle = 7'b0000000;
-        endcase
-        // tile out
-        case(tile_CIU[2:0])  
-            1: ena_out = 7'b0000001;
-            2: ena_out = 7'b0000010;
-            3: ena_out = 7'b0000100;
-            4: ena_out = 7'b0001000;
-            5: ena_out = 7'b0010000;
-            6: ena_out = 7'b0100000;
-            7: ena_out = 7'b1000000;
-            default: ena_out = 7'b0000000;
-        endcase
-    end
-    always@(posedge clka) begin
-        if(rst) begin
-            ena <= 7'd0;
-        end
-        else begin
-            if(en) begin
-                ena = ena_in | en_cycle | ena_out;
-            end
-            else begin
-                ena <= 7'd0;
-            end
-        end
-    end
-
-    // enb
-    reg [6:0] enb_cal_0, enb_cal_1, enb_cal_2, enb_store, enb;
-    always@(*) begin
-        enb = enb_cal_0 | enb_cal_1 | enb_cal_2 | enb_store;
-        // tile cal
-        case(tile_Core[11:9])
-            1: enb_cal_0 = 7'b0000001;
-            2: enb_cal_0 = 7'b0000010;
-            3: enb_cal_0 = 7'b0000100;
-            4: enb_cal_0 = 7'b0001000;
-            5: enb_cal_0 = 7'b0010000;
-            6: enb_cal_0 = 7'b0100000;
-            7: enb_cal_0 = 7'b1000000;
-            default: enb_cal_0 = 7'b0000000;
-        endcase
-        case(tile_Core[8:6])
-            1: enb_cal_1 = 7'b0000001;
-            2: enb_cal_1 = 7'b0000010;
-            3: enb_cal_1 = 7'b0000100;
-            4: enb_cal_1 = 7'b0001000;
-            5: enb_cal_1 = 7'b0010000;
-            6: enb_cal_1 = 7'b0100000;
-            7: enb_cal_1 = 7'b1000000;
-            default: enb_cal_1 = 7'b0000000;
-        endcase
-        case(tile_Core[8:6])
-            1: enb_cal_2 = 7'b0000001;
-            2: enb_cal_2 = 7'b0000010;
-            3: enb_cal_2 = 7'b0000100;
-            4: enb_cal_2 = 7'b0001000;
-            5: enb_cal_2 = 7'b0010000;
-            6: enb_cal_2 = 7'b0100000;
-            7: enb_cal_2 = 7'b1000000;
-            default: enb_cal_2 = 7'b0000000;
-        endcase
-        // tile store
-        case(tile_Core[5:3])
-            1: enb_store = 7'b0000001;
-            2: enb_store = 7'b0000010;
-            3: enb_store = 7'b0000100;
-            4: enb_store = 7'b0001000;
-            5: enb_store = 7'b0010000;
-            6: enb_store = 7'b0100000;
-            7: enb_store = 7'b1000000;
-            default: enb_store = 7'b0000000;
-        endcase
-    end
-    always@(posedge clkb) begin
-        if(rst) begin
-            enb <= 7'd0;
-        end
-        else begin
-            if(en) begin
-                enb = enb_cal_0 | enb_cal_1 | enb_cal_2 | en_cycle | enb_store;
-            end
-            else begin
-                enb <= enb;
-            end
-        end
-    end
-    // 
-    ////////// decode end //////////
-
     ////////// tile 1 //////////
-    wire [63:0] dout_1;
-    Tile_buffer_sdp tile_buffer_1(
+    reg ena_1, enb_1;
+    wire [63:0] douta_1;
+    reg [7:0] addra_1, addrb_1;
+    reg [63:0] dina_1, dinb_1;
+    // mux
+    always@(*) begin
+        case(tile_assign[19:17])
+            1: begin
+                ena_1 = 0;
+                enb_1 = 1;
+                addra_1 = 8'd0;
+                dina_1 = 64'd0;
+                addrb_1 = addr_cal;
+                dinb_1 = 64'd0;
+            end
+            2: begin
+                ena_1 = 1;
+                enb_1 = 0;
+                addra_1 = addr_in;
+                dina_1 = din_in;
+                addrb_1 = 8'd0;
+                dinb_1 = 64'd0;
+            end
+            3: begin
+                ena_1 = 1;
+                enb_1 = 1;
+                addra_1 = addr_cycle_a;
+                dina_1 = din_cycle_a;
+                addrb_1 = addr_cycle_b;
+                dinb_1 = din_cycle_b;
+            end
+            4: begin
+                ena_1 = 0;
+                enb_1 = 1;
+                addra_1 = 8'd0;
+                dina_1 = 64'd0;
+                addrb_1 = addr_store;
+                dinb_1 = din_store;
+            end
+            default: begin
+                ena_1 = 0;
+                enb_1 = 0;
+                addra_1 = 8'd0;
+                dina_1 = 64'd0;
+                addrb_1 = 8'd0;
+                dinb_1 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_1(
         .clka(clka),
-        .ena(ena[0]),
-        .wea(we_in),
-        .addra(addr_in),
-        .dina(data_in),
+        .ena(ena_1),
+        .wea( valid_in | valid_cycle_a ),
+        .addra(addra_1),
+        .dina(dina_1),
+        .douta(douta_1),
         .clkb(clkb),
-        .enb(enb[0]),
-        .addrb(addr_cal),
-        .doutb(dout_1)
+        .enb(enb_1),
+        .web( valid_store | valid_cycle_b ),
+        .addrb(addrb_1),
+        .dinb(dinb_1),
+        .doutb(tile_1)
     );
     ////////// tile 1 end //////////
 
     ////////// tile 2 //////////
-    wire [63:0] dout_2;
-    Tile_buffer_sdp tile_buffer_2(
+    reg ena_2, enb_2;
+    wire [63:0] douta_2;
+    reg [7:0] addra_2, addrb_2;
+    reg [63:0] dina_2, dinb_2;
+    // mux
+    always@(*) begin
+        case(tile_assign[16:14])
+            1: begin
+                ena_2 = 0;
+                enb_2 = 1;
+                addra_2 = 8'd0;
+                dina_2 = 64'd0;
+                addrb_2 = addr_cal;
+                dinb_2 = 64'd0;
+            end
+            2: begin
+                ena_2 = 1;
+                enb_2 = 0;
+                addra_2 = addr_in;
+                dina_2 = din_in;
+                addrb_2 = 8'd0;
+                dinb_2 = 64'd0;
+            end
+            3: begin
+                ena_2 = 1;
+                enb_2 = 1;
+                addra_2 = addr_cycle_a;
+                dina_2 = din_cycle_a;
+                addrb_2 = addr_cycle_b;
+                dinb_2 = din_cycle_b;
+            end
+            4: begin
+                ena_2 = 0;
+                enb_2 = 1;
+                addra_2 = 8'd0;
+                dina_2 = 64'd0;
+                addrb_2 = addr_store;
+                dinb_2 = din_store;
+            end
+            default: begin
+                ena_2 = 0;
+                enb_2 = 0;
+                addra_2 = 8'd0;
+                dina_2 = 64'd0;
+                addrb_2 = 8'd0;
+                dinb_2 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_2(
         .clka(clka),
-        .ena(ena[1]),
-        .wea(we_in),
-        .addra(addr_in),
-        .dina(data_in),
+        .ena(ena_2),
+        .wea( valid_in | valid_cycle_a ),
+        .addra(addra_2),
+        .dina(dina_2),
+        .douta(douta_2),
         .clkb(clkb),
-        .enb(enb[1]),
-        .addrb(addr_cal),
-        .doutb(dout_2)
+        .enb(enb_2),
+        .web( valid_store | valid_cycle_b ),
+        .addrb(addrb_2),
+        .dinb(dinb_2),
+        .doutb(tile_2)
     );
     ////////// tile 2 end //////////
 
     ////////// tile 3 //////////
-    wire [63:0] dout_3;
-    Tile_buffer_sdp tile_buffer_3(
+    reg ena_3, enb_3;
+    wire [63:0] douta_3;
+    reg [7:0] addra_3, addrb_3;
+    reg [63:0] dina_3, dinb_3;
+    // mux
+    always@(*) begin
+        case(tile_assign[13:11])
+            1: begin
+                ena_3 = 0;
+                enb_3 = 1;
+                addra_3 = 8'd0;
+                dina_3 = 64'd0;
+                addrb_3 = addr_cal;
+                dinb_3 = 64'd0;
+            end
+            2: begin
+                ena_3 = 1;
+                enb_3 = 0;
+                addra_3 = addr_in;
+                dina_3 = din_in;
+                addrb_3 = 8'd0;
+                dinb_3 = 64'd0;
+            end
+            3: begin
+                ena_3 = 1;
+                enb_3 = 1;
+                addra_3 = addr_cycle_a;
+                dina_3 = din_cycle_a;
+                addrb_3 = addr_cycle_b;
+                dinb_3 = din_cycle_b;
+            end
+            4: begin
+                ena_3 = 0;
+                enb_3 = 1;
+                addra_3 = 8'd0;
+                dina_3 = 64'd0;
+                addrb_3 = addr_store;
+                dinb_3 = din_store;
+            end
+            default: begin
+                ena_3 = 0;
+                enb_3 = 0;
+                addra_3 = 8'd0;
+                dina_3 = 64'd0;
+                addrb_3 = 8'd0;
+                dinb_3 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_3(
         .clka(clka),
-        .ena(ena[2]),
-        .wea(we_store),
-        .addra(addr_store),
-        .dina(data_store),
+        .ena(ena_3),
+        .wea( valid_in | valid_cycle_a ),
+        .addra(addra_3),
+        .dina(dina_3),
+        .douta(douta_3),
         .clkb(clkb),
-        .enb(enb[2]),
-        .addrb(addr_cal),
-        .doutb(dout_3)
+        .enb(enb_3),
+        .web( valid_store | valid_cycle_b ),
+        .addrb(addrb_3),
+        .dinb(dinb_3),
+        .doutb(tile_3)
     );
     ////////// tile 3 end //////////
 
     ////////// tile 4 //////////
-    wire [63:0] dout_4;
-    Tile_buffer_sdp tile_buffer_4(
+    reg ena_4, enb_4;
+    wire [63:0] douta_4;
+    reg [7:0] addra_4, addrb_4;
+    reg [63:0] dina_4, dinb_4;
+    // mux
+    always@(*) begin
+        case(tile_assign[10:8])
+            1: begin
+                ena_4 = 0;
+                enb_4 = 1;
+                addra_4 = 8'd0;
+                dina_4 = 64'd0;
+                addrb_4 = addr_cal;
+                dinb_4 = 64'd0;
+            end
+            2: begin
+                ena_4 = 1;
+                enb_4 = 0;
+                addra_4 = addr_in;
+                dina_4 = din_in;
+                addrb_4 = 8'd0;
+                dinb_4 = 64'd0;
+            end
+            3: begin
+                ena_4 = 1;
+                enb_4 = 1;
+                addra_4 = addr_cycle_a;
+                dina_4 = din_cycle_a;
+                addrb_4 = addr_cycle_b;
+                dinb_4 = din_cycle_b;
+            end
+            4: begin
+                ena_4 = 0;
+                enb_4 = 1;
+                addra_4 = 8'd0;
+                dina_4 = 64'd0;
+                addrb_4 = addr_store;
+                dinb_4 = din_store;
+            end
+            default: begin
+                ena_4 = 0;
+                enb_4 = 0;
+                addra_4 = 8'd0;
+                dina_4 = 64'd0;
+                addrb_4 = 8'd0;
+                dinb_4 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_4(
         .clka(clka),
-        .ena(ena[3]),
-        .wea(we_store),
-        .addra(addr_store),
-        .dina(data_store),
+        .ena(ena_4),
+        .wea( valid_in | valid_cycle_a ),
+        .addra(addra_4),
+        .dina(dina_4),
+        .douta(douta_4),
         .clkb(clkb),
-        .enb(enb[3]),
-        .addrb(addr_cal),
-        .doutb(dout_4)
+        .enb(enb_4),
+        .web( valid_store | valid_cycle_b ),
+        .addrb(addrb_4),
+        .dinb(dinb_4),
+        .doutb(tile_4)
     );
     ////////// tile 4 end //////////
 
     ////////// tile 5 //////////
-    wire [63:0] dout_5;
-    Tile_buffer_sdp tile_buffer_5(
+    reg ena_5, enb_5;
+    wire [63:0] douta_5;
+    reg [7:0] addra_5, addrb_5;
+    reg [63:0] dina_5, dinb_5;
+    // mux
+    always@(*) begin
+        case(tile_assign[7:5])
+            1: begin
+                ena_5 = 0;
+                enb_5 = 1;
+                addra_5 = 8'd0;
+                dina_5 = 64'd0;
+                addrb_5 = addr_cal;
+                dinb_5 = 64'd0;
+            end
+            2: begin
+                ena_5 = 1;
+                enb_5 = 0;
+                addra_5 = addr_in;
+                dina_5 = din_in;
+                addrb_5 = 8'd0;
+                dinb_5 = 64'd0;
+            end
+            3: begin
+                ena_5 = 1;
+                enb_5 = 1;
+                addra_5 = addr_cycle_a;
+                dina_5 = din_cycle_a;
+                addrb_5 = addr_cycle_b;
+                dinb_5 = din_cycle_b;
+            end
+            4: begin
+                ena_5 = 0;
+                enb_5 = 1;
+                addra_5 = 8'd0;
+                dina_5 = 64'd0;
+                addrb_5 = addr_store;
+                dinb_5 = din_store;
+            end
+            default: begin
+                ena_5 = 0;
+                enb_5 = 0;
+                addra_5 = 8'd0;
+                dina_5 = 64'd0;
+                addrb_5 = 8'd0;
+                dinb_5 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_5(
         .clka(clka),
-        .ena(ena[4]),
-        .wea(we_store),
-        .addra(addr_store),
-        .dina(data_store),
+        .ena(ena_5),
+        .wea( valid_in | valid_cycle_a ),
+        .addra(addra_5),
+        .dina(dina_5),
+        .douta(douta_5),
         .clkb(clkb),
-        .enb(enb[4]),
-        .addrb(addr_cal),
-        .doutb(dout_5)
+        .enb(enb_5),
+        .web( valid_store | valid_cycle_b ),
+        .addrb(addrb_5),
+        .dinb(dinb_5),
+        .doutb(tile_5)
     );
     ////////// tile 5 end //////////
 
     ////////// tile 6 //////////
+    reg ena_6, enb_6;
+    wire [63:0] douta_6;
     reg [7:0] addra_6, addrb_6;
-    reg [63:0] dina_6;
-    wire [63:0] douta_6, doutb_6;
-    always@(*) begin // a
-        if(mux_sel[0]) begin
-            addra_6 = addr_cycle_0;
-            dina_6 = din_cycle_0;
-        end
-        else begin
-            addra_6 = addr_store;
-            dina_6 = din_store;
-        end
-    end
-    always@(*) begin // b
-        case(mux_sel[2:1])
-            2'b00: addrb_6 = addr_cal;
-            2'b01: addrb_6 = addr_cycle_1;
-            2'b10: addrb_6 = addr_out;
-            default: addrb_6 = addr_cal;
+    reg [63:0] dina_6, dinb_6;
+    // mux
+    always@(*) begin
+        case(tile_assign[4:2])
+            1: begin
+                ena_6 = 0;
+                enb_6 = 1;
+                addra_6 = 8'd0;
+                dina_6 = 64'd0;
+                addrb_6 = addr_cal;
+                dinb_6 = 64'd0;
+            end
+            2: begin
+                ena_6 = 1;
+                enb_6 = 0;
+                addra_6 = addr_in;
+                dina_6 = din_in;
+                addrb_6 = 8'd0;
+                dinb_6 = 64'd0;
+            end
+            3: begin
+                ena_6 = 1;
+                enb_6 = 1;
+                addra_6 = addr_cycle_a;
+                dina_6 = din_cycle_a;
+                addrb_6 = addr_cycle_b;
+                dinb_6 = din_cycle_b;
+            end
+            4: begin
+                ena_6 = 0;
+                enb_6 = 1;
+                addra_6 = 8'd0;
+                dina_6 = 64'd0;
+                addrb_6 = addr_store;
+                dinb_6 = din_store;
+            end
+            default: begin
+                ena_6 = 0;
+                enb_6 = 0;
+                addra_6 = 8'd0;
+                dina_6 = 64'd0;
+                addrb_6 = 8'd0;
+                dinb_6 = 64'd0;
+            end
         endcase
     end
+
     Tile_buffer_tdp tile_buffer_6(
         .clka(clka),
-        .ena(ena[5]),
-        .wea(we_in),
+        .ena(ena_6),
+        .wea( valid_in | valid_cycle_a ),
         .addra(addra_6),
         .dina(dina_6),
         .douta(douta_6),
         .clkb(clkb),
-        .enb(enb[5]),
-        .web(we_cycle_1),
+        .enb(enb_6),
+        .web( valid_store | valid_cycle_b ),
         .addrb(addrb_6),
-        .dinb(din_cycle_1),
-        .doutb(doutb_6)
+        .dinb(dinb_6),
+        .doutb(tile_6)
     );
     ////////// tile 6 end //////////
 
     ////////// tile 7 //////////
-    Tile_buffer_sdp tile_buffer_7(
+    reg ena_7, enb_7;
+    wire [63:0] doutb_7;
+    reg [7:0] addra_7, addrb_7;
+    reg [63:0] dina_7;
+    // mux
+    always@(*) begin
+        case(tile_assign[1:0])
+            1: begin // store
+                ena_7 = 1;
+                addra_7 = addr_store;
+                dina_7 = din_store;
+                enb_7 = 0;
+                addrb_7 = 8'd0;
+                dinb_7 = 64'd0;
+            end
+            2: begin // out
+                ena_7 = 0;
+                addra_7 = 8'd0;
+                dina_7 = 64'd0;
+                enb_7 = 1;
+                addrb_7 = addr_out;
+                dinb_7 = 64'd0;
+            end
+            default: begin
+                ena_7 = 0;
+                enb_7 = 0;
+                addra_7 = 8'd0;
+                dina_7 = 64'd0;
+                addrb_7 = 8'd0;
+                dinb_7 = 64'd0;
+            end
+        endcase
+    end
+
+    Tile_buffer_tdp tile_buffer_7(
         .clka(clka),
-        .ena(ena[6]),
-        .wea(wea[6]),
-        .addra(addr_store),
-        .dina(data_store),
+        .ena(ena_7),
+        .wea(valid_store),
+        .addra(addra_7),
+        .dina(dina_7),
         .clkb(clkb),
-        .enb(enb[6]),
-        .addrb(addr_out),
-        .doutb(data_out)
+        .enb(enb_7),
+        .addrb(addrb_7),
+        .doutb(dout_out)
     );
     ////////// tile 7 end //////////
 
-    ////////// cal out //////////
+    ////////// cycle out //////////
     always@(*) begin
-        case()
+        case(tile_sel_cycle)
+            1: dout_cycle = douta_1;
+            2: dout_cycle = douta_2;
+            3: dout_cycle = douta_3;
+            4: dout_cycle = douta_4;
+            5: dout_cycle = douta_5;
+            6: dout_cycle = douta_6;
+            default: dout_cycle = 64'd0;
+        endcase
     end
-    ////////// cal out //////////
+    ////////// cycle out end //////////
 endmodule
