d801c31574adbba0dc012f27ab266853fb3a7c01
DEVAN9105 <devansun299@gmail.com>
2026-01-31 20:37:10 +0800

    Update Node_input_buffer.v

--- CIU/Node_input_buffer.v
+++ CIU/Node_input_buffer.v
@@ -1,6 +1,24 @@
 `timescale 1ns / 1ps
 
 module Node_input_buffer(
-
+    input CLK,
+    input rst,
+    input en,
+    input [71:0] GLB_out,
+    output reg [71:0] node_out
     );
+    
+    always @(posedge CLK) begin
+        if (rst) begin
+            node_out <= 0;
+        end
+        else begin
+            if(en) begin
+                node_out <= GLB_out;
+            end
+            else begin
+                node_out <= node_out;
+            end
+        end
+    end
 endmodule
