af9aa85eee6681c9d4adc02e345681d47c7643c3
DEVAN9105 <devansun299@gmail.com>
2026-02-01 11:03:42 +0800

    Update Bridge_output_buffer.v

--- CIU/Bridge_output_buffer.v
+++ CIU/Bridge_output_buffer.v
@@ -1,25 +1,60 @@
 `timescale 1ns / 1ps
 
-module Node_output_buffer(
+module Bridge_output_buffer(
     input CLK,
     input rst,
-    input en,
+    input en_in,
     input [63:0] dout_out,
-    output reg [63:0] bridge_out
+    output reg [63:0] GLB_in,
+    // addr buffer
+    output en_out,
+    input [7:0] addr_out_in,
+    output reg [7:0] addr_out
     );
 
+    ////////// valid SR //////////
+    reg [2:0] en_SR;
     always @(posedge CLK) begin
         if (rst) begin
-            bridge_out <= 0;
+            en_SR <= 0;
         end
         else begin
-            if (en) begin
-                bridge_out <= dout_out;
+            en_SR <= {en_SR[1:0], en_in};
+        end
+    end
+    assign en_out = en_SR[2];
+    ////////// valid SR end//////////
+
+    ///////// addr buffer //////////
+    always @(posedge CLK) begin
+        if (rst) begin
+            addr_out <= 0;
+        end
+        else begin
+            if (en_in) begin
+                addr_out <= addr_out_in;
             end
             else begin
-                bridge_out <= bridge_out;
+                addr_out <= addr_out;
             end
         end
     end
+    ///////// addr buffer end//////////
+
+    ///////// GLB_in buffer //////////
+    always @(posedge CLK) begin
+        if (rst) begin
+            GLB_in <= 0;
+        end
+        else begin
+            if (en_SR[2]) begin
+                GLB_in <= dout_out;
+            end
+            else begin
+                GLB_in <= GLB_in;
+            end
+        end
+    end
+    ///////// GLB_in buffer end//////////
 
 endmodule
