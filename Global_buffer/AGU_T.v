e6c824ea0596fa3d8f038c1d82ffc93db735ac35
DEVAN9105 <devansun299@gmail.com>
2026-02-02 22:21:34 +0800

    renew

---
+++ Global_buffer/AGU_T.v
@@ -0,0 +1,220 @@
+`timescale 1ns / 1ps
+
+// delay = 3 cycle
+
+module AGU_T(
+    input CLK,
+    input en,
+    input rst,
+    input [7:0] AGU_T_initial_in,
+    input [6:0] tile_width_in,
+    input [7:0] tile_ch_in,
+    input [2:0] core,
+    output reg [2:0] core_pointer,
+    output reg [7:0] taddr,
+    output reg done
+    );
+    
+    ////////// input buffer //////////
+    reg [7:0] AGU_T_initial;
+    reg [6:0] tile_width;
+    reg [7:0] tile_ch;
+    reg [7:0] ch_stride;
+    always@(posedge CLK) begin
+        AGU_T_initial <= AGU_T_initial_in;
+        tile_width <= tile_width_in;
+        tile_ch <= tile_ch_in;
+        ch_stride <= tile_width_in + 1; //stride = tile_width
+    end
+    ////////// input buffer end //////////
+
+    ////////// en SR //////////
+    reg [1:0] en_sr;
+    always@(posedge CLK) begin
+        if(rst) begin
+            en_sr <= 0;
+        end
+        else begin
+            en_sr <= {en_sr[0], en};
+        end
+    end
+    ////////// en SR end //////////
+
+    ////////// stage 1 //////////
+    reg [7:0] X,next_X;
+    reg s1_done;
+    reg s2_en;
+    always@(posedge CLK) begin
+        s2_en <= s1_done & en;
+    end
+    //counter
+    always@(*) begin
+        //avoid latch
+        next_X = X;
+        s1_done = 0;
+        
+        //counter logic
+        if(X == tile_width) begin
+            next_X = 0;
+            s1_done = 1;
+        end
+        else begin
+            next_X = X + 1;
+            s1_done = 0;
+        end
+    end
+    always@(posedge CLK) begin
+        if(rst == 1) begin
+            X <= 0;
+        end
+        else begin
+            if(en) begin
+                X <= next_X;
+            end
+            else begin
+                X <= X;
+            end
+        end
+    end
+
+    // adder
+    reg [7:0] adder_1;
+    always@(posedge CLK) begin
+        if(rst) begin
+            adder_1 <= 0;
+        end
+        else begin
+            if(en) begin
+                adder_1 <= AGU_T_initial + X;
+            end
+            else begin
+                adder_1 <= adder_1;
+            end
+        end
+    end
+    ////////// stage 1 end //////////
+    
+    ////////// stage 2 //////////
+    reg [7:0] ch, next_ch;
+    reg [7:0] Y, next_Y;
+    reg s2_done, s3_en;
+    always@(posedge CLK) begin
+        s3_en <= s2_done & s2_en;
+    end
+    
+    always@(*) begin
+        next_ch = ch;
+        next_Y = Y;
+        s2_done = 0;
+        if(ch == tile_ch) begin
+            next_ch = 0;
+            next_Y = 0;
+            s2_done = 1;
+        end
+        else begin
+            next_ch = ch + 1;
+            next_Y = Y + ch_stride;
+            s2_done = 0;
+        end
+    end
+    always@(posedge CLK) begin
+        if(rst) begin
+            ch <= 0;
+            Y <= 0;
+        end
+        else begin
+            if(s2_en) begin
+                ch <= next_ch;
+                Y <= next_Y;
+            end
+            else begin
+                ch <= ch;
+                Y <= Y;
+            end
+        end
+    end
+
+    // adder
+    reg [7:0] adder_2;
+    always@(posedge CLK) begin
+        if(rst) begin
+            adder_2 <= 0;
+        end
+        else begin
+            if(en_sr[0]) begin
+                adder_2 <= adder_1 + Y;
+            end
+            else begin
+                adder_2 <= adder_2;
+            end
+        end
+    end
+    ////////// stage 2 end //////////
+
+    ////////// stage 3 //////////
+    reg [2:0] core_count, next_core_count;
+    reg s3_done;
+    
+    always@(*) begin
+        next_core_count = core_count;
+        s3_done = 0;
+
+        if(core_count == core) begin
+            next_core_count = 0;
+            s3_done = 1;
+        end
+        else begin
+            next_core_count = core_count + 1;
+            s3_done = 0;
+        end
+    end
+    always@(posedge CLK) begin
+        if(rst) begin
+            core_count <= 0;
+        end
+        else begin
+            if(s3_en) begin
+                core_count <= next_core_count;
+            end
+            else begin
+                core_count <= core_count;
+            end
+        end
+    end
+
+    // reg
+    always@(posedge CLK) begin
+        if(rst) begin
+            taddr <= 0;
+            core_pointer <= 0;
+        end
+        else begin
+            if(en_sr[1]) begin
+                taddr <= adder_2;
+                core_pointer <= core_count;
+            end
+            else begin
+                taddr <= taddr;
+                core_pointer <= core_pointer;
+            end
+        end
+    end
+    ////////// stage 3 end //////////
+
+    ////////// done signal //////////
+    always@(posedge CLK) begin
+        if(rst) begin
+            done <= 0;
+        end
+        else begin
+            if(s3_done && s3_en) begin
+                done <= 1;
+            end
+            else begin
+                done <= 0;
+            end
+        end
+    end
+    ////////// done signal end //////////
+
+endmodule
