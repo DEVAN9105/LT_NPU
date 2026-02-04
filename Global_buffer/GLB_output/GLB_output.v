51dc82d10e85c02b2c4df98540d3763834be88a2
DEVAN9105 <devansun299@gmail.com>
2026-02-04 16:41:30 +0800

    Update GLB_output.v

--- Global_buffer/GLB_output/GLB_output.v
+++ Global_buffer/GLB_output/GLB_output.v
@@ -17,30 +17,28 @@ module GLB_input(
     output [9:0] ch_sum,
     input [11:0] Y,
     output [11:0] gaddr,
-    // input tile
-    output reg [6:0] wb_en, // 0: pre_processing tile, 1~6: core
+    // output tile
+    output load_en,
     output [7:0] taddr,
-    input [63:0] CIU_node_wb_2,
-    input [63:0] CIU_node_wb_5,
-    input [63:0] PP_wb,
-    input en_wb_c2,
-    input en_wb_c5,
-    input en_wb_pp,
+    output [63:0] glb_load,
     // glb control
     output glb_b_en,
+    input [63:0] dout_glb,
     // done signal
     output done
     );
     
     ////////// GLB control //////////
-    wire [9:0] SR;
+    wire [10:0] SR;
     wire glb_out_rst;
+    wire AGU_G_en;
     wire AGU_G_done;
     GLB_output_controller glb_output_controller(
         .CLK(CLK),
         .en(en),
         .rst(rst),
         .AGU_G_done(AGU_G_done),
+        .AGU_G_en(AGU_G_en),
         .SR(SR),
         .done(done),
         .glb_out_rst(glb_out_rst)
@@ -49,30 +47,14 @@ module GLB_input(
 
     ////////// signal assign //////////
     assign glb_b_en = SR[0];
+    assign load_en = SR[7];
     ////////// signal assign end //////////
-
-    ////////// AGU_T //////////
-    reg [2:0] core;
-    wire [2:0] core_pointer;
-    AGU_T agu_t(
-    .CLK(CLK),
-    .en(SR_0[0]),
-    .rst(rst),
-    .AGU_T_initial_in(AGU_T_initial_in),
-    .tile_width_in(tile_width_in),
-    .tile_ch_in(tile_ch_in),
-    .core(core),
-    .core_pointer(core_pointer),
-    .taddr(taddr),
-    .done(AGU_T_done)
-    );
-    ////////// AGU_T end //////////
 
     ////////// AGU_G //////////
     AGU_G agu_g(
         .CLK(CLK),
-        .en(SR[1]),
-        .rst(rst),
+        .en(AGU_G_en),
+        .rst(glb_out_rst),
         .AGU_G_initial_in(AGU_G_initial_in),
         .glb_width_in(glb_width_in),
         .glb_ch_in(glb_ch_in),
@@ -83,14 +65,30 @@ module GLB_input(
     );
     ////////// AGU_G end //////////
 
-    ////////// Input Transpose //////////
+    ////////// AGU_T //////////
+    reg [2:0] core;
+    wire [2:0] core_pointer;
+    AGU_T agu_t(
+        .CLK(CLK),
+        .en(SR[3]),
+        .rst(glb_out_rst),
+        .AGU_T_initial_in(AGU_T_initial_in),
+        .tile_width_in(tile_width_in),
+        .tile_ch_in(tile_ch_in),
+        .core(core),
+        .core_pointer(core_pointer),
+        .taddr(taddr)
+    );
+    ////////// AGU_T end //////////
+
+    ////////// Output Transpose //////////
     Transpose transpose(
         .CLK(CLK),
-        .rst(rst),
-        .en(SR_1[2]),
+        .rst(glb_out_rst),
+        .en(SR[3]),
         .data(data_buffer_1),
         .data_transpose(din_glb)
     );
-    ////////// Input Transpose end //////////
+    ////////// Output Transpose end //////////
 
 endmodule
