09fab38afafaa4506935d025c27c21015bc35cca
DEVAN9105 <devansun299@gmail.com>
2026-02-06 16:43:59 +0800

    renew

---
+++ Controller/VLIW_FSM.v
@@ -0,0 +1,113 @@
+`timescale 1ns / 1ps
+
+module VLIW_FSM(
+    input CLK,
+    input en,
+    input rst,
+    // done mux
+    input complete,
+    // PC
+    output reg PC_en,
+    input PC_done,
+    // control
+    output reg VLIW_done,
+    output reg VLIW_rst
+    );
+    
+    ////////// state define //////////
+    parameter idle = 0, set_up = 1, run = 2, finish = 3;
+    ////////// state define end //////////
+
+    ////////// control counter //////////
+    reg [4:0] control_count;
+    reg cc_en;
+    always@(posedge CLK) begin
+        if(rst) begin
+            control_count <= 0;
+        end
+        else begin
+            if(cc_en) begin
+                control_count <= control_count + 1;
+            end
+            else begin
+                control_count <= 0;
+            end
+        end
+    end
+    ////////// control counter end //////////
+
+    ////////// FSM //////////
+    reg [1:0] state, next_state;
+    always@(*) begin
+        //avoid latch
+        next_state = state;
+        VLIW_done = 0;
+        cc_en = 0;
+        PC_en = 0;
+        VLIW_rst = 0;
+
+        case(state)
+            idle: begin
+                VLIW_rst = 1;
+                if(en) begin
+                    next_state = set_up;
+                end
+                else begin
+                    next_state = idle;
+                end
+            end
+            set_up: begin
+                cc_en = 1;
+                // next state
+                if(control_count == 5) begin
+                    next_state = run;
+                end
+                else begin
+                    next_state = set_up;
+                end
+                // PC + 1
+                if(control_count == 1) begin
+                    PC_en = 1;
+                end
+                else begin
+                    PC_en = 0;
+                end
+            end
+            run: begin
+                if(complete) begin
+                    if(PC_done) begin
+                        next_state = finish;
+                    end
+                    else begin
+                        next_state = set_up;
+                    end
+                end
+                else begin
+                    next_state = run;
+                end
+            end
+            finish: begin
+                VLIW_done = 1;
+                if(en) begin
+                    next_state = finish;
+                end
+                else begin
+                    next_state = idle;
+                end
+            end
+            default: begin
+                next_state = idle;
+            end
+        endcase
+    end
+    always@(posedge CLK) begin
+        if(rst) begin
+            state <= idle;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+    ////////// FSM end //////////
+
+endmodule
