ccdf28b30afdf1f0538c0dd9bc089bc9ccdd9bdc
DEVAN9105 <devansun299@gmail.com>
2026-02-23 00:50:49 +0800

    Update LT_NPU.v

--- PL_assenbly/LT_NPU.v
+++ PL_assenbly/LT_NPU.v
@@ -11,7 +11,14 @@ module LT_NPU(
     output PL_busy,
 
     ////////// DMA and Memory interface //////////
-    
+    input  wire [63:0] s_axis_tdata_Weight,
+    input  wire        s_axis_tvalid_Weight,
+    output wire        s_axis_tready_Weight,
+
+    input  wire [63:0]  s_axis_tdata_Inmage,
+    input  wire         s_axis_tvalid_Inmage,
+    input  wire         s_axis_tlast_Inmage,
+    output wire         s_axis_tready_Inmage,
 
     ////////// output //////////
     output [3:0] inference_result
