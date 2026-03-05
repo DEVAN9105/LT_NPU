def71801ef2b069f991ad5fe4bd034e21cf51679
DEVAN9105 <devansun299@gmail.com>
2026-03-05 20:18:49 +0800

    renew

---
+++ Core_TBO_CIU/TBO/Tile_buffer_sdp.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+
+(* DONT_TOUCH = "TRUE" *)
+module Tile_buffer_sdp (
+    ///// Port A (Read/Write) /////
+    input clka,
+    input ena,
+    input wea,
+    input [7:0] addra,
+    input [63:0] dina,
+    
+    ///// Port B (Read/Write) /////
+    input clkb,
+    input enb,
+    input web,           // Port B 寫入 Enable
+    input regceb,        // Port B 輸出暫存器 Enable
+    input [7:0] addrb,
+    output [63:0] doutb  // Port B 讀取資料輸出
+);
+
+    // -------------------------------------------------------------------------
+    // XPM_MEMORY_SDPRAM: Simple Dual Port RAM for Block RAM
+    // -------------------------------------------------------------------------
+    xpm_memory_sdpram #(
+        .ADDR_WIDTH_A(8),               // Port A 地址寬度: 8 bits (2^8 = 256)
+        .ADDR_WIDTH_B(8),               // Port B 地址寬度: 8 bits (2^8 = 256)
+        .BYTE_WRITE_WIDTH_A(64),         // Port A 寫入寬度 (不使用 Byte Enable)
+        .BYTE_WRITE_WIDTH_B(64),         // Port B 寫入寬度 (不使用 Byte Enable)
+        .CLOCKING_MODE("independent_clock"),  // two Clock
+        .ECC_MODE("no_ecc"),             // 不需要 ECC
+        .MEMORY_INIT_FILE("none"),       
+        .MEMORY_OPTIMIZATION("true"),    // 讓 Vivado 自動優化
+        .MEMORY_PRIMITIVE("block"),      // ★★★ 關鍵：改為 "block" (BRAM) ★★★
+        .MEMORY_SIZE(16384),             // 總容量 bits = 64 * 256 = 16,384
+        .MESSAGE_CONTROL(0),
+        .READ_DATA_WIDTH_A(64),          // Port A 讀取寬度
+        .READ_DATA_WIDTH_B(64),          // Port B 讀取寬度
+        .READ_LATENCY_B(2),              // Port B 讀取延遲 (含外部 reg)
+        .USE_MEM_INIT(0),
+        .WAKEUP_TIME("disable_sleep"),
+        .WRITE_DATA_WIDTH_A(64),         // Port A 寫入寬度
+        .WRITE_MODE_B("read_first")
+    )
+    xpm_memory_sdpram_inst (
+        // Common Clock & Reset
+        .clka(clka),
+        .clkb(clkb),
+        .rstb(1'b0),          // BRAM Port B Reset
+
+        // Port A
+        .ena(ena),
+        .wea(wea),
+        .addra(addra),
+        .dina(dina),
+
+        // Port B
+        .enb(enb),         
+        .addrb(addrb),
+        .doutb(doutb),
+        .regceb(regceb),
+        
+        // others
+        .sleep(1'b0),
+        .injectsbiterra(1'b0),
+        .injectdbiterra(1'b0),
+        .injectsbiterrb(1'b0),
+        .injectdbiterrb(1'b0)
+    );
+
+endmodule
